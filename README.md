9f3408bf75861bfb2cc149885cfd8637732826f9
Jimmy-7664 <47916283+Jimmy-7664@users.noreply.github.com>
2025-05-23 15:01:02 +0900

    Update ST-SSDL

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # ST-SSDL
+An implementation of ST-SSDL under GCRU backbone.
+
+
+* Run following commands to prepare data:
+
+  ```bash
+  python generate_training_data_his_BAY.py
+  python generate_training_data_his_LA.py
+  python generate_training_data_his_PEMS.py --dataset PEMS04
+  python generate_training_data_his_PEMS.py --dataset PEMS07
+  python generate_training_data_his_PEMS.py --dataset PEMS08
+  python generate_training_data_his_D7.py --dataset PEMSD7M
+  ```
+
+* Then train the model with following commands:
+
+  ```bash
+  cd model_MDGCRN
+  python train_STSSDL.py --gpu 1 --dataset METRLA
+  python train_STSSDL.py --gpu 1 --dataset PEMSBAY
+  python train_STSSDL.py --gpu 1 --dataset PEMSD7M
+  python train_STSSDL.py --gpu 1 --dataset PEMS04
+  python train_STSSDL.py --gpu 1 --dataset PEMS07
+  python train_STSSDL.py --gpu 1 --dataset PEMS08
+
+  ```
+
+  
---
+++ scripts/run1.sh
@@ -0,0 +1,40 @@
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate traffic_flow
+
+cd model_DGCRN1
+
+# temperature
+for t in 10.0 5.0 3.0 2.0 1.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9; do
+    python traintorch_MDGCRN.py \
+    --contra_loss=infonce \
+    --temp=$t
+done
+
+# # InfoNCE Loss
+# for t in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 2.0 3.0; do
+# for lambda in 0.01 0.05 0.1 0.5 1.0 2.0; do
+# for lambda1 in 0.01 0.05 0.1 0.5 1.0 2.0; do
+#     python traintorch_MDGCRN.py \
+#         --contra_loss=infonce \
+#         --temp=$t \
+#         --lamb=$lambda \
+#         --lamb1=$lambda1 \
+#         --gpu=1
+
+# done
+# done
+# done
+
+# # Triplet Loss
+# for lambda in 0.01 0.05 0.1 0.5 1.0 2.0; do
+# for lambda1 in 0.01 0.05 0.1 0.5 1.0 2.0; do
+#     python traintorch_MDGCRN.py \
+#         --contra_loss=triplet \
+#         --lamb=$lambda \
+#         --lamb1=$lambda1 \
+#         --gpu=1
+
+# done
+# done
+  
+
